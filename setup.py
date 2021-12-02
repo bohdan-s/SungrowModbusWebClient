bc3086bb5bad67574236897cd973c59c7a3ae492
bohdan-s <bohdan-s@users.noreply.github.com>
2021-12-02 15:10:42 +1100

    Bump version with fixed requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SungrowModbusWebClient",
-    version="0.2.0",
+    version="0.2.1",
     author="Bohdan Flower",
     author_email="github@bohdan.net",
     description="A ModbusWebClient wrapper for talking to Sungrow WiNet-S Dongle via HTTP API",
